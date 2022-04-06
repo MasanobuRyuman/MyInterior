60965bea158609f6b2b78e1371e150dae82f16d0
ryumanmasanobu <22011376mr@tama.ac.jp>
2022-04-06 23:13:24 +0900

    コンテンツページのヘッダー作成

---
+++ src/components/ui/templates/ContentPageTemplate.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import {ContentPageHeader} from './../organisms'
+
+export const ContentPageTemplate = () =>{
+  return(
+    <div>
+      <ContentPageHeader/>
+    </div>
+  )
+}
