01fd1a5bdc1f2f66bd678946e45ca4a64a91d892
ryumanmasanobu <22011376mr@tama.ac.jp>
2022-04-10 11:04:41 +0900

    オブジェクトの位置を変更するボタンの作成

--- src/components/ui/organisms/AR.js
+++ src/components/ui/organisms/AR.js
@@ -10,9 +10,13 @@ import { Box } from '@mui/material'
 import { useContext, useState } from 'react'
 
 import { ObjectDataContext } from './../../../context/ObjectDataContext'
-import { DefaultButton, H3 } from '../atoms'
+import { DefaultButton } from '../atoms'
 
 export const AR = () => {
+  const [ObjectSize, setObjectSize] = useState(5)
+  const [ObjectSizeData, setObjectSizeData] = useState('5 5 5')
+  const [ObjectPosition, setObjectPosition] = useState([0, 0, 0])
+  const [ObjectPositionData, setObjectPositionData] = useState('0 0 0')
   const [vectorData, setVectorData] = useState('0 0 0')
   const [rotationData, setRotationData] = useState(
     matrix([
@@ -21,8 +25,6 @@ export const AR = () => {
       [0, 0, 1],
     ])
   )
-  const [ObjectSize, setObjectSize] = useState(5)
-  const [ObjectSizeData, setObjectSizeData] = useState('5 5 5')
 
   const { ObjectURL, MaterialURL } = useContext(ObjectDataContext)
   const rotation = (move) => {
@@ -37,38 +39,38 @@ export const AR = () => {
       [0, 1, 0],
       [0, 0, 1],
     ])
-    if (move == 'xp') {
+    if (move == 'turnLeftSideWays') {
       mat = matrix([
         [1, 0, 0],
         [0, 0.924, -0.382],
         [0, 0.382, 0.924],
       ])
-    } else if (move == 'xm') {
+    } else if (move == 'turnRightSideWays') {
       mat = matrix([
         [1, 0, 0],
         [0, 0.924, 0.382],
         [0, -0.382, 0.924],
       ])
-    } else if (move == 'yp') {
+    } else if (move == 'turnBottom') {
       mat = matrix([
         [0.924, 0, 0.382],
         [0, 1, 0],
         [-0.382, 0, 0.924],
       ])
-    } else if (move == 'ym') {
+    } else if (move == 'turnUp') {
       mat = matrix([
         [0.924, 0, -0.382],
         [0, 1, 0],
         [0.382, 0, 0.924],
       ])
-    } else if (move == 'zp') {
-      console.log('zp')
+    } else if (move == 'turnLeft') {
+      console.log('turnLeft')
       mat = matrix([
         [0.924, -0.382, 0],
         [0.382, 0.924, 0],
         [0, 0, 1],
       ])
-    } else if (move == 'zm') {
+    } else if (move == 'turnRight') {
       mat = matrix([
         [0.924, 0.382, 0],
         [-0.382, 0.924, 0],
@@ -90,42 +92,105 @@ export const AR = () => {
   }
 
   const ChangeObjectSize = (value) => {
+    console.log(ObjectSizeData)
     if (value == 'up') {
+      console.log('kita')
+      console.log(ObjectSize + 1)
       setObjectSize(ObjectSize + 1)
+      setObjectSizeData(`${ObjectSize + 1} ${ObjectSize + 1} ${ObjectSize + 1}`)
     } else if (value == 'down') {
+      console.log('kita')
+      console.log(ObjectSize - 1)
       setObjectSize(ObjectSize - 1)
+      setObjectSizeData(`${ObjectSize - 1} ${ObjectSize - 1} ${ObjectSize - 1}`)
     }
-    console.log(ObjectSize)
-    setObjectSizeData(ObjectSize + ' ' + ObjectSize + ' ' + ObjectSize)
-    console.log(setObjectSizeData)
+  }
+
+  const ChangePosition = (value) => {
+    console.log(ObjectPositionData)
+    if (value == 'down') {
+      setObjectPosition([
+        ObjectPosition[0],
+        ObjectPosition[1],
+        ObjectPosition[2] + 1,
+      ])
+      setObjectPositionData(
+        `${ObjectPosition[0]} ${ObjectPosition[1]} ${ObjectPosition[2] + 1}`
+      )
+    } else if (value == 'up') {
+      setObjectPosition([
+        ObjectPosition[0],
+        ObjectPosition[1],
+        ObjectPosition[2] - 1,
+      ])
+      setObjectPositionData(
+        `${ObjectPosition[0]} ${ObjectPosition[1]} ${ObjectPosition[2] - 1}`
+      )
+    } else if (value == 'left') {
+      setObjectPosition([
+        ObjectPosition[0] - 1,
+        ObjectPosition[1],
+        ObjectPosition[2],
+      ])
+      setObjectPositionData(
+        `${ObjectPosition[0] - 1} ${ObjectPosition[1]} ${ObjectPosition[2]}`
+      )
+    } else if (value == 'right') {
+      setObjectPosition([
+        ObjectPosition[0] + 1,
+        ObjectPosition[1],
+        ObjectPosition[2],
+      ])
+      setObjectPositionData(
+        `${ObjectPosition[0] + 1} ${ObjectPosition[1]} ${ObjectPosition[2]}`
+      )
+    } else if (value == 'back') {
+      setObjectPosition([
+        ObjectPosition[0],
+        ObjectPosition[1] - 1,
+        ObjectPosition[2],
+      ])
+      setObjectPositionData(
+        `${ObjectPosition[0]} ${ObjectPosition[1] - 1} ${ObjectPosition[2]}`
+      )
+    } else if (value == 'near') {
+      setObjectPosition([
+        ObjectPosition[0],
+        ObjectPosition[1] + 1,
+        ObjectPosition[2],
+      ])
+      setObjectPositionData(
+        `${ObjectPosition[0]} ${ObjectPosition[1] + 1} ${ObjectPosition[2]}`
+      )
+    }
   }
 
   return (
     <Box>
       <Box
-        id="a-frame"
+        id='a-frame'
         sx={{
           width: 1400,
           height: 1400,
         }}
       >
-        <a-scene embedded arjs="debugUIEnabled:false;">
+        <a-scene embedded arjs='debugUIEnabled:false;'>
           <a-assets>
-            <a-asset-item id="cow" src={ObjectURL}></a-asset-item>
-            <a-asset-item id="mtl" src={MaterialURL}></a-asset-item>
+            <a-asset-item id='cow' src={ObjectURL}></a-asset-item>
+            <a-asset-item id='mtl' src={MaterialURL}></a-asset-item>
           </a-assets>
-          <a-marker preset="hiro">
+          <a-marker preset='hiro'>
             <a-obj-model
-              src="#cow"
-              mtl="#mtl"
+              src='#cow'
+              mtl='#mtl'
               scale={ObjectSizeData}
-              position="0 1 0"
+              position={ObjectPositionData}
               rotation={vectorData}
             ></a-obj-model>
           </a-marker>
           <a-entity camera></a-entity>
         </a-scene>
-        
+
         <Box
           sx={{
             position: 'absolute',
@@ -133,14 +198,14 @@ export const AR = () => {
             top: 30,
           }}
         >
-          <a href="../content">
+          <a href='../content'>
             <DefaultButton>戻る</DefaultButton>
           </a>
         </Box>
         <Box
           sx={{
             position: 'absolute',
-            top: '80%',
+            top: '70%',
             left: 10,
           }}
         >
@@ -155,32 +220,53 @@ export const AR = () => {
           sx={{
             position: 'absolute',
             display: 'flex',
+            top: '80%',
+            left: 10,
+          }}
+        >
+          <DefaultButton onClick={() => ChangePosition('down')}>
+            手前
+          </DefaultButton>
+          <DefaultButton onClick={() => ChangePosition('up')}>奥</DefaultButton>
+          <DefaultButton onClick={() => ChangePosition('left')}>
+            左
+          </DefaultButton>
+          <DefaultButton onClick={() => ChangePosition('right')}>
+            右
+          </DefaultButton>
+          <DefaultButton onClick={() => ChangePosition('back')}>
+            下
+          </DefaultButton>
+          <DefaultButton onClick={() => ChangePosition('near')}>
+            上
+          </DefaultButton>
+        </Box>
+        <Box
+          sx={{
+            position: 'absolute',
+            display: 'flex',
             top: '90%',
             left: 10,
           }}
         >
-          <Box>
-            <DefaultButton onClick={() => rotation('yp')}>下回り</DefaultButton>
-          </Box>
-          <Box>
-            <DefaultButton onClick={() => rotation('ym')}>上周り</DefaultButton>
-          </Box>
-          <Box>
-            <DefaultButton onClick={() => rotation('zp')}>左回り</DefaultButton>
-          </Box>
-          <Box>
-            <DefaultButton onClick={() => rotation('zm')}>右回り</DefaultButton>
-          </Box>
-          <Box>
-            <DefaultButton onClick={() => rotation('xp')}>
-              左横まわり
-            </DefaultButton>
-          </Box>
-          <Box>
-            <DefaultButton onClick={() => rotation('xm')}>
-              右横まわり
-            </DefaultButton>
-          </Box>
+          <DefaultButton onClick={() => rotation('turnBottom')}>
+            下回り
+          </DefaultButton>
+          <DefaultButton onClick={() => rotation('turnUp')}>
+            上周り
+          </DefaultButton>
+          <DefaultButton onClick={() => rotation('turnLeft')}>
+            左回り
+          </DefaultButton>
+          <DefaultButton onClick={() => rotation('turnRight')}>
+            右回り
+          </DefaultButton>
+          <DefaultButton onClick={() => rotation('turnLeftSideWays')}>
+            左横まわり
+          </DefaultButton>
+          <DefaultButton onClick={() => rotation('turnRightSideWays')}>
+            右横まわり
+          </DefaultButton>
         </Box>
       </Box>
     </Box>
